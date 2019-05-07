94136df1db8bf434cd3e830c5b84b7783837319f
Nemanja Lazic <nemanja.lazic87@gmail.com>
2019-05-07 21:49:11 +0200

    small fix

--- pg1/app.js
+++ pg1/app.js
@@ -1,33 +1,6 @@
-let getClickClassName = () => {
-    let min=1, max=100;  
-    let random = Math.random() * (+max - +min) + +min; 
-    if (random > 50) {
-        return 'click-left';        
-    } else {
-        return 'click-right';
-    }
-}
-
-let ahrefs = document.getElementsByTagName("li");
-for (let item of ahrefs) {
-    item.onmouseover = (e)=> {
-        let className = getClickClassName();
-        e.target.classList.add(className);
-        setTimeout(() => {
-            if (e.target.classList.contains(className)) {
-                e.target.classList.remove(className);
-            }
-        }, 1000);
-    };
-
+let list = document.getElementsByTagName("li");
+for (let item of list) {
     item.onclick = (e)=> {
-        let className = 'cl';
-        e.target.classList.add(className);
-        setTimeout(() => {
-            if (e.target.classList.contains(className)) {
-                e.target.classList.remove(className);
-            }
-        }, 1000);
+        console.log('click!');
     };
-
 }
